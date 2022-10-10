69caa37a3931307c628a59aa6151576647e18893
tvinnie <tvinnie77@gmail.com>
2022-10-10 15:08:59 +0300

    Debugging ESlint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ export default function Navbar() {
         <header className='bg-gray-800 md:sticky top-0 z-10'>
             <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
                 <img className='logo-img' alt='personal-logo' src='./snap.jpg'/>
-                <a className='title-font font-medium text-white mb-4 md:mb-0'>
+                <h5 className='title-font font-medium text-white mb-4 md:mb-0'>
                     <a href='#about' className='ml-3 text-xl'> Vincent </a>
-                </a>
-                
+                </h5>
+
                 <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center'>
                 <a href='#projects' className='mr-5  hover:text-white'>Projects</a>
                 <a href='#skills' className='mr-5 hover:text-white'>Skills</a>
-                <a href='#testimonials' className='mr:5 hover:text-white'>Testimonials</a>
+                <a href='#testimonials' className='mr-5 hover:text-white'>Testimonials</a>
                 </nav>
 
                 <a href='#contact' className='inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base nt-4 md:mt-0'>
