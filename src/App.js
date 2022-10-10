55e20442d48350335cbe00abf42f9b1a57f0be96
tvinnie <tvinnie77@gmail.com>
2022-10-10 14:02:23 +0300

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import About from './components/About';
+import Contact from './components/Contact';
+
+import Navbar from "./components/Navbar";
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import {ArrowRightIcon} from '@heroicons/react/solid';
+import Testimonials from "./components/Testimonials";
+
+
+ export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Testimonials/>
+      <Contact/>
+    </main>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const projects = [
+    {
+        title: "MoFlix",
+        subtitle: "React | OMDb API | Bootstrap",
+        description:
+          "A Movie app that fetches the movies from OMDB api, converts to JSON objects and displays to the user",
+        image: "./mof.gif",
+        link: "https://github.com/tvinnie/MoFlix",
+      },
+      {
+        title: "Adventure",
+        subtitle: "React | React Router DOM",
+        description:
+          "Simple Responsive React App",
+        image: "./adv.gif",
+        link: "https://github.com/tvinnie/Adventure",
+      },
+      {
+        title: "Flashpoint",
+        subtitle: "HTML | CSS | JS",
+        description:"A Responsive site where one can order a pizza from the comfort of there home",
+        image: "./flash.gif",
+        link: "https://github.com/tvinnie/Flashpoint",
+      },
+      {
+        title: "Epuka App",
+        subtitle: "Android | Firebase |Java | Material UI",
+        description:
+        "Designed a scalable, robust and secure mobile app where addicts in remission can find help.",
+        image: "./epuka.png",
+        link: "https://github.com/tvinnie/epuka",
+      },
+]
+export const testimonials = [
+    {
+        quote:
+          "Mr Kipruto is goal oriented, a team player and determined young man. He demonstrated ingenuity in software development. He was well organized and thrived under adversity. In many instances we placed him in situations that we felt only he could succeed, and he never let us down. Without exception, he put the customer first; felling as we do, that the key to  good business is customer satisfaction.",
+        image: "https://raw.githubusercontent.com/tvinnie/master/master/img/mikeutaf.jpg",
+        name: "Dr. Michael Jairo",
+        company: "Senior Consultant at Utafiti International",
+      },
+      {
+        quote:
+          "Having worked with Too Vincent and assessed output, i fully testify he is good in the following areas of programming: back end developer of Java and PHP, User interface development, MySQL, Javascript and server side processing. I do recommend him in any future consideration.",
+        image: "https://raw.githubusercontent.com/tvinnie/master/master/img/kims.jpg",
+        name: "Kimatia Joshua Daniel",
+        company: "Software Developer",
+      },
+      {
+        quote:
+          "Vincent is a problem solver, he provided adept technical support to colleagues and the organization, he is also very good at java script. I highly recommend him for any tech-related task.",
+        image: "https://raw.githubusercontent.com/tvinnie/master/master/img/lily.png",
+        name: "Lilian Nyambura",
+        company: "Kenafric Industries Ltd",
+      },
+      {
+        quote:
+          "A great guy, with a brilliant mind. All things UI/UX expect him to give you something unique.",
+        image: "https://raw.githubusercontent.com/tvinnie/master/master/img/kevin.jpg",
+        name: "Kevin Tum",
+        company: "Peak and Dale Solutions Limited",
+      },
+]
+export const skills = [
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node",
+    "Version Control",
+    "Material UI",
+    "Android",
+    "Java"
+]
