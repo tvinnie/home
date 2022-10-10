b2bd30c9eeff9f9e9af482d795b64fcdd40fcdfd
tvinnie <tvinnie77@gmail.com>
2022-10-10 15:30:41 +0300

    Rearrange Skillsets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ export default function Projects() {
                         Projects
                     </h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        Sone of the Apps i have developed are:
+                        Some of the Apps i have developed are:
                     </p>
                 </div>
                 <div className="flex flex-wrap m-4">
