bfda11b7f27468daaef20e54ee17316b65f1ca6b
tvinnie <tvinnie77@gmail.com>
2022-10-10 15:37:47 +0300

    rearrange about section

--- src/components/About.js
+++ src/components/About.js
@@ -17,12 +17,12 @@ export default function About() {
                         <a 
                         href="#contact"
                         className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                      See My Past Work
+                            Contact
                         </a>
                         <a
                         href="#projects"
                         className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                        Projects
+                        See My Past Work
                         </a>
                     </div>
                 </div>
